fd475437580fbd60909e668731c017a877f56e63
Fahim044 <fahiimfaysalleusc044@gmail.com>
2025-11-16 21:33:05 +0600

    added update export Product

--- index.js
+++ index.js
@@ -60,6 +60,23 @@ async function run() {
         res.send(result);
 
     })
+    app.patch('/products/:id',async(req,res)=>{
+        const id=req.params.id;
+        const query={_id:new ObjectId(id)};
+        const updatedProduct=req.body;
+        const update={
+            $set:{
+name:updatedProduct.name,
+image:updatedProduct.image,
+price:updatedProduct.price,
+originCountry:updatedProduct.originCountry,
+rating:updatedProduct.rating,
+availableQuantity:updatedProduct.availableQuantity,
+            }
+        };
+        const result=await productsCollection.updateOne(query,update);
+        res.send(result);
+    })
     app.delete('/products/:id',async(req,res)=>{
         const id=req.params.id;
         const query={_id:new ObjectId(id)};
