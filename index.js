46344ae1fc8c9e229cbf2a2538f8534c16a94b75
Fahim044 <fahiimfaysalleusc044@gmail.com>
2025-11-16 18:44:36 +0600

    added my export delete functionality

--- index.js
+++ index.js
@@ -60,6 +60,12 @@ async function run() {
         res.send(result);
 
     })
+    app.delete('/products/:id',async(req,res)=>{
+        const id=req.params.id;
+        const query={_id:new ObjectId(id)};
+        const result=await productsCollection.deleteOne(query);
+        res.send(result);
+    })
     // import related APIs
     app.get('/imports',async(req,res)=>{
         const email=req.query.email;
